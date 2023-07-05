eba4d5dbe46c670cfba624e30408313a8b67ed31
thomas <thomas@stafe.fr>
2023-07-05 09:31:36 +0200

    Added one package to be installed

--- install_all.sh
+++ install_all.sh
@@ -1,2 +1,2 @@
-sudo npm i -g pyright intelephense bash-language-server tree-sitter typescript typescript-language-server
+sudo npm i -g pyright intelephense bash-language-server tree-sitter typescript typescript-language-server python3-pylsp
 sudo apt install efm-langserver shellcheck
